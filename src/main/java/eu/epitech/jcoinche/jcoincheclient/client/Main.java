9eb4b358d3e3bb5258700669859958306d3ef5fa
peauc <clement.peau@epitech.eu>
2017-10-17 16:06:20 +0200

    Hello maven :D

---
+++ src/main/java/eu/epitech/jcoinche/jcoincheclient/client/Main.java
@@ -0,0 +1,7 @@
+package eu.epitech.jcoinche.jcoincheclient.client;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("HelloClient");
+    }
+}
