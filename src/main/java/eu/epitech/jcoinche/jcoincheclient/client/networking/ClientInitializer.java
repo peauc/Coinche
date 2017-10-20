c61fc3ee7679ba8139d5720236cc1f387021d50f
peauc <clement.peau@epitech.eu>
2017-10-20 16:03:29 +0200

    Hello server from clientˆ

---
+++ src/main/java/eu/epitech/jcoinche/jcoincheclient/client/networking/ClientInitializer.java
@@ -0,0 +1,32 @@
+package eu.epitech.jcoinche.jcoincheclient.client.networking;
+
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.codec.Delimiters;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+import io.netty.handler.ssl.SslContext;
+
+import io.netty.channel.socket.SocketChannel;
+
+public class ClientInitializer extends ChannelInitializer<SocketChannel> {
+    private final SslContext ssc;
+
+    public ClientInitializer(SslContext sslCtx) {
+        this.ssc = sslCtx;
+    }
+
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+        if (ssc != null) {
+            //pipeline.addLast(ssc.newHandler(ch.alloc(), Connection.getPort(), Integer.parseInt(Connection.getHost())));
+        }
+        pipeline.addLast("framer", new DelimiterBasedFrameDecoder(8192, Delimiters.lineDelimiter()));
+        pipeline.addLast("decoder", new StringDecoder());
+        pipeline.addLast("encoder", new StringEncoder());
+        pipeline.addLast("clientHandler", new ClientHandler(ssc));
+    }
+}
